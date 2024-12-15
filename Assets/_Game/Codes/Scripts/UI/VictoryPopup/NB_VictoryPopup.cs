cef6092f9ad26ac2d635f87f9cfeba4cf763eca3
ProC <hieutg244@gmail.com>
2024-12-15 10:51:59 +0700

    update

--- Assets/_Game/Codes/Scripts/UI/VictoryPopup/NB_VictoryPopup.cs
+++ Assets/_Game/Codes/Scripts/UI/VictoryPopup/NB_VictoryPopup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,6 +9,7 @@ namespace NultBolts
 {
     public class NB_VictoryPopup : MonoBehaviour
     {
+        public static int reward = 100;
         [Header("Setup")]
         [SerializeField] TextMeshProUGUI txt_ValueReward;
         [SerializeField] Button btn_Next;
@@ -15,13 +17,19 @@ namespace NultBolts
         private void Start()
         {
             btn_Next.onClick.AddListener(Next);
+
+            txt_ValueReward.text = reward.ToString();
+        }
+
+        private void OnEnable()
+        {
+            GameDataManager.Instance.playerData.AddDiamond(reward);
         }
 
         void Next()
         {
             NultBoltsManager.Instance.ReloadLevel();
             gameObject.SetActive(false);
-
         }
     }
 }
---
+++ Assets/_InApp/RainSound/Scripts/UI/IAPScreen.cs
@@ -0,0 +1,20 @@
+﻿using BabySound.Scripts;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BabySound
+{
+    public class IAPScreen : MonoBehaviour
+    {
+        [SerializeField] private BuyCoinButton[] _buttons;
+
+        void Start()
+        {
+            for (int i = 0; i < _buttons.Length; i++)
+            {
+                _buttons[i].Index = i;
+            }
+        }
+    }
+}
